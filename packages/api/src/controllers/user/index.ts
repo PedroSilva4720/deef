e3ddd1a33d74dc305ec26cf4768689eee03a0938
Pedro Silva <pedrosilva@MacBook-Pro-de-Pedro.local>
2022-10-25 21:46:29 -0300

    feat(api): user CRUD
    functions to create user, update name and password and delete user.

---
+++ packages/api/src/controllers/user/index.ts
@@ -0,0 +1,91 @@
+import { Request, Response } from 'express';
+import { hash } from 'argon2';
+
+import { prisma } from '../../prisma';
+import { credentialsDecoderByBasicAuth } from './services';
+
+export const createUserByCompanyId = async (req: Request, res: Response) => {
+  const { name } = req.body;
+  const companyId = req.params.companyId;
+  const basicAuth = req.headers.authorization;
+
+  const { email, password } = credentialsDecoderByBasicAuth(basicAuth);
+
+  const passwordHash = await hash(password);
+
+  try {
+    await prisma.user.create({
+      data: {
+        name,
+        email,
+        passwordHash,
+        createdAt: new Date(),
+        companyId,
+      },
+    });
+
+    return res.json({ message: 'Usuário criado com sucesso!' });
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+export const updateUserName = async (req: Request, res: Response) => {
+  const { newUserName } = req.body;
+  const id = req.params.id;
+
+  try {
+    await prisma.user.update({
+      where: {
+        id,
+      },
+      data: {
+        name: newUserName,
+      },
+    });
+
+    return res.json({ message: 'Usuário atualizado com sucesso!' });
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+export const updateUserPassowrd = async (req: Request, res: Response) => {
+  const basicAuth = req.headers.authorization;
+  const id = req.params.id;
+
+  const { password } = credentialsDecoderByBasicAuth(basicAuth);
+
+  const passwordHash = await hash(password);
+
+  try {
+    await prisma.user.update({
+      where: {
+        id,
+      },
+      data: {
+        passwordHash,
+      },
+    });
+
+    return res.json({ message: 'Senha atualizada com sucesso!' });
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+export const deleteUser = async (req: Request, res: Response) => {
+  const id = req.params.id;
+
+  try {
+    await prisma.user.delete({
+      where: {
+        id,
+      },
+    });
+
+    res.json({ message: 'Usuário excluído com sucesso!' });
+  } catch (error) {
+    throw new Error(error);
+  }
+};
--- packages/api/src/router.ts
+++ packages/api/src/router.ts
@@ -1,5 +1,7 @@
+import { updateUserPassowrd, updateUserName } from './controllers/user/index';
 import { Request, Response, Router } from 'express';
 import { createCompany } from './controllers/company';
+import { createUserByCompanyId, deleteUser } from './controllers/user';
 
 export const router = Router();
 
@@ -7,4 +9,11 @@ router.get('/', (req: Request, res: Response) => {
   res.send('Olá mundo2');
 });
 
+//company routes
 router.post('/company', createCompany);
+
+//manager (user) routes
+router.post('/user/:companyId', createUserByCompanyId);
+router.patch('/user/name/:id', updateUserName);
+router.patch('/user/password/:id', updateUserPassowrd);
+router.delete('/user/:id', deleteUser);
